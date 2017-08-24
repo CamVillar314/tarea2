62754650a84b148932c3639f809f81f3c0b4a0a2
CamVillar314 <30761171+CamVillar314@users.noreply.github.com>
2017-08-24 17:43:36 -0300

    Add file

---
+++ Polinomios.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.Scanner;
+import java.lang.*;
+
+public class Polinomios{
+
+
+	public static int[] suma(int[] a, int[] b){
+
+		int largoArrays = 
+		for (int k = 0; ; ) {
+			
+		}
+	}
+
+	public static int[] producto(int[] c, int d){
+
+	}
+
+
+	public static void main(String[] args) {
+
+
+		Scanner m = new Scanner(System.in);
+		int n = Integer.parseInt(m);
+		int[] pol1 = new int[n+1];
+		int[] pol2 = new int[n+1];
+
+		for (int i = 0; i < n+1 ; ++i) {
+
+			Scanner a1 = new Scanner(System.in);
+			int a = Integer.paseInt(a1);
+			pol1[i] = a;
+
+		}
+
+		for (int j = 0 ; j < n+1 ; ++j) {
+
+			Scanner b1 = new Scanner(System.in);
+			int b = Integer.paseInt(b1);
+			pol2[j] = b;
+		}
+
+
+
+		
+	}
+}
