84254760d187ad03de80d83cec9da779be7f82b1
CamVillar314 <30761171+CamVillar314@users.noreply.github.com>
2017-08-30 16:09:02 -0300

    Update

--- Polinomios.java
+++ Polinomios.java
@@ -5,15 +5,27 @@ import java.lang.*;
 public class Polinomios{
 
 
-	public static int[] suma(int[] a, int[] b){
-
-		int largoArrays = 
-		for (int k = 0; ; ) {
+	public static int[] suma(int[] a, int[] b, int c){
+		
+		int[] sumaArrays = new int[c];
+		for (int k = 0; k < c ; ++k) {
 			
+			sumaArrays[k] += a[k] + b[k];
 		}
+		return sumaArrays;
 	}
 
-	public static int[] producto(int[] c, int d){
+	public static int[] producto(int[] c, int[] d, int e){
+		
+		int[] productoArrays = new int[2*e-2];
+		for (int l = 0; l < e; ++l){
+			for (int m = 0; m < e; ++m){
+				
+				productoArrays[l + m] = c[l] * b[m];
+			}
+			
+		}
+		return productoArrays;
 
 	}
 
@@ -22,14 +34,14 @@ public class Polinomios{
 
 
 		Scanner m = new Scanner(System.in);
-		int n = Integer.parseInt(m);
+		int n = m.nextInt();
 		int[] pol1 = new int[n+1];
 		int[] pol2 = new int[n+1];
 
 		for (int i = 0; i < n+1 ; ++i) {
 
 			Scanner a1 = new Scanner(System.in);
-			int a = Integer.paseInt(a1);
+			int a = a1.nextInt();
 			pol1[i] = a;
 
 		}
@@ -37,7 +49,7 @@ public class Polinomios{
 		for (int j = 0 ; j < n+1 ; ++j) {
 
 			Scanner b1 = new Scanner(System.in);
-			int b = Integer.paseInt(b1);
+			int b = b1.nextInt();
 			pol2[j] = b;
 		}
 
