58e9f01b076325f1a57c15a5a9519e69a2867ea9
CamVillar314 <30761171+CamVillar314@users.noreply.github.com>
2017-08-31 20:29:12 -0300

    Update

--- Polinomios.java
+++ Polinomios.java
@@ -10,9 +10,19 @@ public class Polinomios{
 		int[] sumaArrays = new int[c];
 		for (int k = 0; k < c ; ++k) {
 			
-			sumaArrays[k] += a[k] + b[k];
+			sumaArrays[k] = a[k] + b[k];
 		}
 		return sumaArrays;
+	}
+
+	public static int[] resta(int[] a, int[] b, int c){
+
+		int[] restaArrays = new int[c];
+		for (int k = 0; k < c; ++k) {
+			
+			restaArrays[k] = a[k] + b[k];
+		}
+		return restaArrays;
 	}
 
 	public static int[] producto(int[] c, int[] d, int e){
@@ -21,7 +31,7 @@ public class Polinomios{
 		for (int l = 0; l < e; ++l){
 			for (int m = 0; m < e; ++m){
 				
-				productoArrays[l + m] = c[l] * b[m];
+				productoArrays[l + m] = c[l] * d[m];
 			}
 			
 		}
@@ -29,12 +39,38 @@ public class Polinomios{
 
 	}
 	
-	public static int[] d(int[] polin1, int[] polin2, int f){
-	
-		
+	public static int[] resultado(int[] polin1, int[] polin2, int[] polin3, int[] polin4, int g){
+
+
+		//Primero se calcula el polinomio d
+		int[] d1 = new int[g];
+		d1 = suma(polin1,polin2,g);
+		int[] d2 = new int[g];
+		d2 = suma(polin3,polin4,g);
+		int[] d = new int[2*g-2];
+		d = producto(d1,d2,g);
+
+		//Calcula el polinomio e
+		int[] e = new int[2*g-2];
+		e = producto(polin1,polin3,g);
+
+		//Calcula el polinomio f
+		int[] f = new int[2*g-2];
+		f = producto(polin2,polin4,g);
+
+		//Calcula el polinomio resultante
+		int[] c = new int[4*g];
+		for (i = 0; i < 2*g-2; ++i) {
+			
+			c[i] = e[i];
+		}
+		for (j = 2*g-2; ; ) {
+			
+		}
+
+
+
 	}
-	
-	
 
 	public static void main(String[] args) {
 
@@ -58,24 +94,25 @@ public class Polinomios{
 			int b = b1.nextInt();
 			pol2[j] = b;
 		}
-		
-		int[] aprim = new int[(n+1)/2];
-		int [] a2prim = new int[(n+1)/2];
-		int[] bprim = new int[(n+1)/2];
-		int[] b2prim = new int[(n+1)/2];
-		for (int i = 0; i < (n+1)/2; ++i){
+		int ble = (n+1)/2;
+
+		int[] aprim = new int[ble];
+		int [] a2prim = new int[ble];
+		int[] bprim = new int[ble];
+		int[] b2prim = new int[ble];
+		for (int i = 0; i < ble; ++i){
 			
-			aprim[i] = poli1[i];
-			bprim[i] = poli2[i];
+			aprim[i] = pol1[i];
+			bprim[i] = pol2[i];
 		}
-		for(int j = (n+1)/2; j < (n+1); ++j){
+		for(int j = ble; j < (n+1); ++j){
 		
-			a2prim[j] = poli1[j];
-			b2prim[j] = poli2[j];
+			a2prim[j] = pol1[j];
+			b2prim[j] = pol2[j];
 		}
 
 
-
+		System.out.println(resultado(aprim,a2prim,bprim,b2prim,ble));
 		
 	}
 }
